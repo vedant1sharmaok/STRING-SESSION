2c6f76d19f7cbf5f619a9a5c82bc6dbec6bbb824
vedant1sharmaok <135007288+vedant1sharmaok@users.noreply.github.com>
2025-05-19 18:01:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,5 +9,5 @@ API_HASH = getenv("API_HASH", "9f7cb9215fa0007089951c86d31d0128")
 BOT_TOKEN = getenv("BOT_TOKEN", "7084577439:AAE9EekElY9fCcuVd-JaxLJ44Iaw2ePbGLw")
 OWNER_ID = int(getenv("OWNER_ID", "5685358346"))
 
-MONGO_DB_URI = getenv("MONGO_DB_URI", ")
+MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://vedant12ok:vedant@cluster0.hknrwrr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 MUST_JOIN = getenv("MUST_JOIN", None)
