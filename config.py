e059162c7e65949971fa5c558c1c43c805897e33
vedant1sharmaok <135007288+vedant1sharmaok@users.noreply.github.com>
2025-05-19 18:00:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,11 +3,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
+API_ID = int(getenv("API_ID","23053980"))
+API_HASH = getenv("API_HASH", "9f7cb9215fa0007089951c86d31d0128")
 
-BOT_TOKEN = getenv("BOT_TOKEN")
-OWNER_ID = int(getenv("OWNER_ID"))
+BOT_TOKEN = getenv("BOT_TOKEN", "7084577439:AAE9EekElY9fCcuVd-JaxLJ44Iaw2ePbGLw")
+OWNER_ID = int(getenv("OWNER_ID", "5685358346"))
 
-MONGO_DB_URI = getenv("MONGO_DB_URI")
+MONGO_DB_URI = getenv("MONGO_DB_URI", ")
 MUST_JOIN = getenv("MUST_JOIN", None)
