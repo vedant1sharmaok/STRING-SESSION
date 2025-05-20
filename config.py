eb7edf32dce7b5dec2bd494e2582e67bbea4c6c9
vedant1sharmaok <135007288+vedant1sharmaok@users.noreply.github.com>
2025-05-20 20:13:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ load_dotenv()
 API_ID = int(getenv("API_ID","23053980"))
 API_HASH = getenv("API_HASH", "9f7cb9215fa0007089951c86d31d0128")
 
-BOT_TOKEN = getenv("BOT_TOKEN", "7084577439:AAE9EekElY9fCcuVd-JaxLJ44Iaw2ePbGLw")
+BOT_TOKEN = getenv("BOT_TOKEN")
 OWNER_ID = int(getenv("OWNER_ID", "5685358346"))
 
 MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://vedant12ok:vedant@cluster0.hknrwrr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
